42a138ae39c92df3c14420a51c662ed3945f4086
Adityaraj6437 <adityaraj766711@gmail.com>
2025-07-25 10:49:37 +0530

    kjkj

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectDB();
 // Middlewares
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://engineering-resource-management-fro.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
